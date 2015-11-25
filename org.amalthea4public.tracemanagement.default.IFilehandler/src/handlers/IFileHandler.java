33c487a6c670f7e70ee5f520fe3ce3a55d696e3d
Salome Maro <salomem@chalmers.se>
2015-11-25 13:08:31 +0100

    Re-factoring of default layer

---
+++ org.amalthea4public.tracemanagement.default.IFilehandler/src/handlers/IFileHandler.java
@@ -0,0 +1,31 @@
+package handlers;
+
+
+import java.util.Optional;
+
+import org.amalthea4public.tracemanagement.generic.artifacts.ArtifactWrapper;
+import org.amalthea4public.tracemanagement.generic.artifacts.ArtifactWrapperContainer;
+import org.amalthea4public.tracemanagement.generic.artifacts.ArtifactsFactory;
+import org.amalthea4public.tracemanagement.generic.handlers.ArtifactHandler;
+import org.amalthea4public.tracemanagement.generic.helpers.ArtifactHelper;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.emf.ecore.EObject;
+
+public class IFileHandler implements ArtifactHandler {
+
+	@Override
+	public boolean canHandleSelection(Object selection) {
+		return selection instanceof IFile;
+	}
+
+	@Override
+	public EObject getEObjectForSelection(Object selection, Optional<ArtifactWrapperContainer> existingWrappers) {
+		IFile selectionAsFile = (IFile) selection;
+		ArtifactWrapper wrapper = ArtifactsFactory.eINSTANCE.createArtifactWrapper();
+		wrapper.setName(selectionAsFile.getName());
+		wrapper.setUri(selectionAsFile.getFullPath().toString());
+		wrapper.setArtifactHandler(this.getClass().getName());
+		
+		return ArtifactHelper.existingWrapperWithURIorNew(wrapper, existingWrappers);
+	}
+}
