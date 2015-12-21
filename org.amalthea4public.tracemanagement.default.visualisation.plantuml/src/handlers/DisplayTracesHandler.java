5e2b25e91dbf439967724111357316ff87ca0f3f
Anthony Anjorin <tonyanjorin@googlemail.com>
2015-12-21 12:32:24 +0100

    added perspective and icons

--- org.amalthea4public.tracemanagement.default.visualisation.plantuml/src/handlers/DisplayTracesHandler.java
+++ org.amalthea4public.tracemanagement.default.visualisation.plantuml/src/handlers/DisplayTracesHandler.java
@@ -6,12 +6,14 @@ import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
-public class DisplayTraceMatrixHandler extends AbstractHandler {
+public class DisplayTracesHandler extends AbstractHandler {
+
+	public static final String PLANT_UML_VIEW_ID = "net.sourceforge.plantuml.eclipse.views.PlantUmlView";
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {
-			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("net.sourceforge.plantuml.eclipse.views.PlantUmlView");		
+			PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(PLANT_UML_VIEW_ID);		
 		} catch (PartInitException e) {
 			e.printStackTrace();
 		}
