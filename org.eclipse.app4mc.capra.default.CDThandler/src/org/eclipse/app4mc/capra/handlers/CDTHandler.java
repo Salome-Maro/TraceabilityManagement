bf16c2cc694bc3f91150697f5066f8d8a53fdc2a
Salome Maro <salomem@chalmers.se>
2016-08-02 15:35:51 +0200

    Added EPL license header

--- org.eclipse.app4mc.capra.default.CDThandler/src/org/eclipse/app4mc/capra/handlers/CDTHandler.java
+++ org.eclipse.app4mc.capra.default.CDThandler/src/org/eclipse/app4mc/capra/handlers/CDTHandler.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *  Copyright (c) {2016} Chalmers|Gothenburg University and rt-labs.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *   Contributors:
+ *      Chalmers|Gothenburg University, rt-labs - initial API and implementation and initial documentation
+ *******************************************************************************/
 package org.eclipse.app4mc.capra.handlers;
 
 import org.eclipse.app4mc.capra.core.adapters.ArtifactMetaModelAdapter;
